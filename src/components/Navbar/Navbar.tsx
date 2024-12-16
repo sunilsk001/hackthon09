2263bc875327c09ab94e5895934414cc5195e734
Sunilsk001 <skshona04@gmail.com>
2024-12-16 20:51:06 +0500

    hackthon done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
           {/* Logo */}
           <Link
             href="/"
-            className="text-[20px] lg:mx-0 mx-44 sm:text-[24px] leading-[32px] font-bold text-white font-helvetica "
+            className="text-[20px] lg:mx-0 md:mx-44 mx-40 sm:text-[24px] leading-[32px] font-bold text-white font-helvetica "
           >
             Food<span className="text-[#FF9F0D]">tuck</span>
           </Link>
